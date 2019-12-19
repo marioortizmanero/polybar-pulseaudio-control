16347009919f94452d740568f40830a7d105532c
Mario Ortiz Manero <marioortizmanero@gmail.com>
2019-12-19 18:57:10 +0100

    Consistency improvements and more documentation

--- pulseaudio-control.sh
+++ pulseaudio-control.sh
@@ -19,73 +19,93 @@ NOTIFICATIONS="no"  # Notifications when switching sinks if enabled
 SINK_BLACKLIST=(  )  # Index blacklist for sinks when switching between them
 
 # Environment constants
-LANGUAGE=en_US  # Functions of this scripts depends on English outputs of pactl
+LANGUAGE=en_US  # Some calls in this script depend on English outputs of pactl
 
 # Global script variables
 isMuted="no"
 activeSink=""
 endColor="%{F-}"
 
-function getCurVol {
-    curVol=$(pacmd list-sinks | grep -A 15 'index: '"$activeSink"'' | grep 'volume:' | grep -E -v 'base volume:' | awk -F : '{print $3}' | grep -o -P '.{0,3}%'| sed s/.$// | tr -d ' ')
+
+function getCurVol() {
+    curVol=$(pacmd list-sinks | grep -A 15 'index: '"$activeSink"'' | grep 'volume:' | grep -E -v 'base volume:' | awk -F : '{print $3}' | grep -o -P '.{0,3}%' | sed s/.$// | tr -d ' ')
 }
 
-function getCurSink {
+
+function getCurSink() {
     activeSink=$(pacmd list-sinks | awk '/* index:/{print $3}')
 }
 
-function volMuteStatus {
-    isMuted=$(pacmd list-sinks | grep -A 15 "index: $activeSink" | awk '/muted/{ print $2}')
+
+function volMuteStatus() {
+    isMuted=$(pacmd list-sinks | grep -A 15 "index: $activeSink" | awk '/muted/{print $2}')
 }
 
-function getSinkInputs {
+
+function getSinkInputs() {
     inputArray=$(pacmd list-sink-inputs | grep -B 4 "sink: $1 " | awk '/index:/{print $2}')
 }
 
-function volUp {
+
+function volUp() {
+    # Obtaining the current volume from pacmd into $curVol.
     getCurVol
     maxLimit=$((MAX_VOL - INC))
 
+    # Checking the volume upper bounds so that if MAX_VOL was 100% and the
+    # increase percentage was 3%, a 99% volume would top at 100% instead
+    # of 102%. If the volume is above the maximum limit, nothing is done.
     if [ "$curVol" -le "$MAX_VOL" ] && [ "$curVol" -ge "$maxLimit" ]; then
         pactl set-sink-volume "$activeSink" "$MAX_VOL%"
     elif [ "$curVol" -lt "$maxLimit" ]; then
         pactl set-sink-volume "$activeSink" "+$INC%"
     fi
 
+    # Updating the current volume global variable for future usage.
     getCurVol
 
-    if [ ${OSD} = "yes" ]; then
+    if [ $OSD = "yes" ]; then
         qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
     fi
 
-    if [ ${AUTOSYNC} = "yes" ]; then
+    if [ $AUTOSYNC = "yes" ]; then
         volSync
     fi
 }
 
-function volDown {
+
+function volDown() {
+    # Pactl already handles the volume lower bounds so that negative values
+    # are ignored.
     pactl set-sink-volume "$activeSink" "-$INC%"
+
+    # Updating the current volume global variable for future usage.
     getCurVol
 
-    if [ ${OSD} = "yes" ]; then
+    if [ $OSD = "yes" ]; then
         qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
     fi
 
-    if [ ${AUTOSYNC} = "yes" ]; then
+    if [ $AUTOSYNC = "yes" ]; then
         volSync
     fi
 }
 
-function volSync {
+
+function volSync() {
     getSinkInputs "$activeSink"
     getCurVol
 
+    # Every output found in the active sink has their volume set to the
+    # current one. This will only be called if $AUTOSYNC is `yes`.
     for each in $inputArray; do
         pactl set-sink-input-volume "$each" "$curVol%"
     done
 }
 
-function volMute {
+
+function volMute() {
+    # Switch to mute the volume with pactl.
     case "$1" in
         mute)
             pactl set-sink-mute "$activeSink" 1
@@ -99,21 +119,22 @@ function volMute {
             ;;
     esac
 
-    if [ ${OSD} = "yes" ]; then
+    if [ $OSD = "yes" ]; then
         qdbus org.kde.kded /modules/kosd showVolume ${curVol} ${status}
     fi
 
 }
 
-function changeDevice {
+
+function changeDevice() {
     # Treat pulseaudio sink list to avoid calling pacmd list-sinks twice
-    o_pulseaudio=$(pacmd list-sinks| grep -e 'index' -e 'device.description')
+    sinksList=$(pacmd list-sinks | grep -e 'index' -e 'device.description')
 
     # Get all sink indices in a list
-    sinks=($(echo "$o_pulseaudio" | grep index | awk -F': ' '{print $2}'))
+    sinks=($(echo "$sinksList" | grep index | awk -F': ' '{print $2}'))
 
     # Get present default sink index
-    activeSink=$(echo "$o_pulseaudio" | grep "\* index" | awk -F': ' '{print $2}')
+    activeSink=$(echo "$sinksList" | grep "\* index" | awk -F': ' '{print $2}')
 
     # Set new sink index, checks that it's not in the blacklist
     newSink=$activeSink
@@ -151,31 +172,32 @@ function changeDevice {
     fi
 }
 
-function sendNotification {
-    o_pulseaudio=$(pacmd list-sinks| grep -e 'index' -e 'device.description')
-    deviceName=$(echo "$o_pulseaudio" | sed -n '/* index/{n;p;}' | grep -o '".*"' | sed 's/"//g')
-    notify-send "Output cycle" "Changed output to ${deviceName}" --icon=audio-headphones-symbolic
+
+function sendNotification() {
+    # Sending a notification when the output changes. This is only called when
+    # $NOTIFICATIONS is set to `yes`.
+    deviceName=$(pacmd list-sinks | grep -e 'index' -e 'device.description' | sed -n '/* index/{n;p;}' | grep -o '".*"' | sed 's/"//g')
+    notify-send "Output cycle" "Changed output to $deviceName" --icon=audio-headphones-symbolic
 }
 
-function listen {
-    firstrun=0
 
+function listen() {
+    firstRun=0
+
+    # Listen for changes and immediately create new output for the bar.
+    # This is faster than having the script on an interval.
     LANG=$LANGUAGE pactl subscribe 2>/dev/null | {
         while true; do
             {
-                # If this is the first time just continue
-                # and print the current state
-                # Otherwise wait for events
-                # This is to prevent the module being empty until
-                # an event occurs
-                if [ $firstrun -eq 0 ]
-                then
-                    firstrun=1
+                # If this is the first time just continue and print the current
+                # state. Otherwise wait for events. This is to prevent the
+                # module being empty until an event occurs.
+                if [ $firstRun -eq 0 ]; then
+                    firstRun=1
                 else
                     read -r event || break
-                    if ! echo "$event" | grep -e "on card" -e "on sink" -e "on server"
-                    then
-                        # Avoid double events
+                    # Avoid double events
+                    if ! echo "$event" | grep -e "on card" -e "on sink" -e "on server"; then
                         continue
                     fi
                 fi
@@ -184,6 +206,7 @@ function listen {
         done
     }
 }
+
 
 function output() {
     if [ -z "$(pgrep pulseaudio)" ]; then echo "Pulseaudio not running"; return 1; fi
@@ -233,8 +256,7 @@ case "$1" in
         ;;
     --togmute)
         volMuteStatus
-        if [ "$isMuted" = "yes" ]
-        then
+        if [ "$isMuted" = "yes" ]; then
             volMute unmute
         else
             volMute mute
@@ -250,16 +272,13 @@ case "$1" in
         volSync
         ;;
     --listen)
-        # Listen for changes and immediately create new output for the bar
-        # This is faster than having the script on an interval
         listen
         ;;
     --change)
-        # Changes the audio device
         changeDevice
         ;;
     *)
-        # By default print output for bar
+        # By default print output for bar.
         output
         ;;
 esac
