191fbd1d0936725cbb8ae6ad36d327412b91507c
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-05-08 00:58:46 +0200

    fix help message

--- pulseaudio-control.bash
+++ pulseaudio-control.bash
@@ -337,40 +337,44 @@ function usage() {
 Usage: $0 [OPTION...] ACTION
 
 Options: [defaults]
-  --autosync | --no-autosync            whether to maintain same volume for all
-                                        programs [$AUTOSYNC]
-  --color-muted <rrggbb>                color in which to format when muted
-                                        [${COLOR_MUTED:4:-1}]
-  --notifications | --no-notifications  whether to show notifications when
-                                        changing sinks [$NOTIFICATIONS]
-  --osd | --no-osd                      whether to display KDE's OSD message
-                                        [$OSD]
-  --icon-muted <icon>                   icon to use when muted [none]
-  --icon-sink <icon>                    icon to use for sink [none]
-  --format <string>                     use a format string to control the output
-                                        Available variables: \$VOL_ICON,
-                                        \$VOL_LEVEL, \$ICON_SINK, and
-                                        \$SINK_NICKNAME
-                                        [$FORMAT]
-  --icons-volume <icon>[,<icon>...]     icons for volume, from lower to higher
-                                        [none]
-  --volume-max <int>                    maximum volume to which to allow
-                                        increasing [$VOLUME_MAX]
-  --volume-step <int>                   step size when inc/decrementing volume
-                                        [$VOLUME_STEP]
-  --sink-blacklist <name>[,<name>...]   sinks to ignore when switching [none]
-  --sink-nicknames-from <prop>          pactl property to use for sink names,
-                                        unless overriden by --sink-nickname.
-                                        Its possible values are listed under
-                                        the 'Properties' key in the output of
-                                        \`pactl list sinks\` [none]
-  --sink-nickname <name>:<nick>         nickname to assign to given sink name,
-                                        taking priority over
-                                        --sink-nicknames-from. May be given
-                                        multiple times, and 'name' is exactly as
-                                        listed in the output of
-                                        \`pactl list sinks short | cut -f2\`
-                                        [none]
+  --autosync | --no-autosync            
+        Whether to maintain same volume for all programs [$AUTOSYNC]
+  --color-muted <rrggbb>                
+        Color in which to format when muted [${COLOR_MUTED:4:-1}]
+  --notifications | --no-notifications  
+        Whether to show notifications when changing sinks [$NOTIFICATIONS]
+  --osd | --no-osd                      
+        Whether to display KDE's OSD message [$OSD]
+  --icon-muted <icon>                   
+        Icon to use when muted [none]
+  --icon-sink <icon>                    
+        Icon to use for sink [none]
+  --format <string>                     
+        Use a format string to control the output.
+        Available variables:
+        * \$VOL_ICON
+        * \$VOL_LEVEL
+        * \$ICON_SINK
+        * \$SINK_NICKNAME
+        [$FORMAT]
+  --icons-volume <icon>[,<icon>...]     
+        Icons for volume, from lower to higher [none]
+  --volume-max <int>                    
+        Maximum volume to which to allow increasing [$VOLUME_MAX]
+  --volume-step <int>                   
+        Step size when inc/decrementing volume [$VOLUME_STEP]
+  --sink-blacklist <name>[,<name>...]   
+        Sinks to ignore when switching [none]
+  --sink-nicknames-from <prop>          
+        pactl property to use for sink names, unless overriden by
+        --sink-nickname. Its possible values are listed under the 'Properties'
+        key in the output of \`pactl list sinks\` [none]
+  --sink-nickname <name>:<nick>         
+        Nickname to assign to given sink name, taking priority over
+        --sink-nicknames-from. May be given multiple times, and 'name' is
+        exactly as listed in the output of \`pactl list sinks short | cut -f2\`.
+        Note that you can also specify a port name for the sink with
+        \`<name>/<port>\`. [none]
 
 Actions:
   help              display this message and exit
